c8b063fafd5bab7d2f86ca402848a9c906edbd10
jayaharisai5 <100357364+jayaharisai5@users.noreply.github.com>
2022-08-28 14:19:23 +0530

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 def pre_processing():
     data_frame_copy = feature_engineering()     #importing the data from the feature_engineering.py file
-    #print(data_frame_copy)                         #checking the data by printing
+    print(data_frame_copy)                         #checking the data by printing
 
     #diviting the required variable and depending variable
     X = data_frame_copy.drop(["LUNG_CANCER"],axis=1)      #required variable
@@ -16,7 +16,7 @@ def pre_processing():
     X_train_res, y_train_res = over_samp.fit_resample(X, y)
     #X_train_res.shape, y_train_res.shape
 
-    #print(X_train_res)
+    print(X_train_res)
     #return(X_train_res, y_train_res)
     #splitting the data to the ring and test set
     X_train, X_test, y_train, y_test = train_test_split(X_train_res, y_train_res,test_size= 0.2, random_state = 0)
