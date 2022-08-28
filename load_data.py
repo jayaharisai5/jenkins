13a35b2264a1a8ed0745dcfc6a49a27a8d8a9179
jayaharisai5 <100357364+jayaharisai5@users.noreply.github.com>
2022-08-28 14:04:01 +0530

    importing the file csv from s3

--- load_data.py
+++ load_data.py
@@ -1,10 +1,19 @@
 # import required libraries
 import pandas as pd
+import boto3
+
+session = boto3.Session( 
+         aws_access_key_id='AKIA2LYVCCXT5J4TG5V7', 
+         aws_secret_access_key='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')
+
+client = boto3.client('s3')
+path = 's3://mlops-storage/hari/survey lung cancer.csv'
 
 
 # importing the data
 def load_data():
-    data = pd.read_csv("survey lung cancer.csv")  # loading the dataset to dataframe
+    data = pd.read_csv(path)
+    #data = pd.read_csv("survey lung cancer.csv")  # loading the dataset to dataframe
     #print(data)
     return data
 #helollo
